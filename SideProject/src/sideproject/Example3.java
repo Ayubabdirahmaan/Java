f6c58817694cc937565a880bd0bd25de56f8d05a
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-08-21 19:30:48 +0300

    Added

--- SideProject/src/sideproject/Example3.java
+++ SideProject/src/sideproject/Example3.java
@@ -4,11 +4,18 @@ public class Example3 {
 //        for(int x=0; x<=10; x+=1) {
 //            System.out.print(x + ",");
 //        }
-        int x=10;
-        while(x>=1) {
-            System.out.print(x +",");
-            x-=1;
-        }
+//        int x=10;
+//        while(x>=1) {
+//            System.out.print(x +",");
+//            x-=1;
+//        }
+
+         for(int x=1; x<=10; x+=1){
+             for(int y=1; y<=10; y+=1) 
+                 System.out.print(x  + "\t");
+                 System.out.println( " ");
+              
+             }
     }
     
 }
