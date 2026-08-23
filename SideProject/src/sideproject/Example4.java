0e05d21cb4b9d0f9d4d9449093b892328807b2f1
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-08-23 21:34:39 +0300

    Added

--- SideProject/src/sideproject/Example4.java
+++ SideProject/src/sideproject/Example4.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sideproject;
 
-/**
- *
- * @author hp
- */
+import java.util.Scanner;
+
 public class Example4 {
+    Scanner kb = new Scanner(System.in)
+    public Example4() {
+        
+    }
     
 }
