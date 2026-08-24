354d23933811fd470ff8b84b91b1d85d8f6c6d43
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-08-24 10:37:07 +0300

    added

--- SideProject/src/sideproject/Example4.java
+++ SideProject/src/sideproject/Example4.java
@@ -1,27 +1,19 @@
-
 package sideproject;
-
 import java.util.Scanner;
-
 public class Example4 {
     Scanner kb = new Scanner(System.in);
     public Example4() {
-        System.out.println("Enter fist number:");
-        int x= kb.nextInt();
-        System.out.println("Enter last number:");
-        int y= kb.nextInt();
+        System.out.println("Enter You age:");
+        int age = kb.nextInt();
+        System.out.println("Enter you Score");
+        int score = kb.nextInt();
         
-        if(x> y ) {
-            int score = x;
-            x=y;
-            y= score;
+        if(age  < 18) {
+            if(score < 60) {
+                System.out.println( age < 18 ? "You are child" : "Your score is low"  );
+            }
+        }else {
+            System.out.println("Welcome again to university ");
         }
-        
-        for(int n=x; n<=y;  n+=1) {
-            System.out.print(n + ",");
-        }
-        
-        
     }
-    
 }
