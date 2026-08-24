a05223d8f541645ea722b167861fbfd2767660d8
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-08-24 13:56:44 +0300

    Added

--- SideProject/src/sideproject/Example4.java
+++ SideProject/src/sideproject/Example4.java
@@ -3,20 +3,22 @@ import java.util.Scanner;
 public class Example4 {
     Scanner kb = new Scanner(System.in);
     public Example4() {
-        System.out.println("Enter Number");
+        System.out.println("Enter Number 1");
         int number = kb.nextInt();
+        System.out.println("Enter Number 2");
+        int num = kb.nextInt();
         System.out.println("1.isku dar \n2.kala jar \n3.isku dhufo");
         int max = kb.nextInt();
         
         switch(max) {
             case 1:
-                System.out.println( number + number);
+                System.out.println( number + num);
                 break;
             case 2:
-                System.out.println("number - number");
+                System.out.println(number - num);
                 break;
             case 3:
-                System.out.println(number * number);
+                System.out.println(number * num);
                 break;
             default:
                 System.out.println("Macsalaamo");
