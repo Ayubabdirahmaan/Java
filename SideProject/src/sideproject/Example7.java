94db8cfdcff76e4e51463b5a26c771f36bf5de80
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-08-26 13:36:39 +0300

    Added

--- SideProject/src/sideproject/Example7.java
+++ SideProject/src/sideproject/Example7.java
@@ -3,7 +3,7 @@ public class Example7 {
     public Example7() {
         int y=10;
         for(int x=y; x>0; x-=1) 
-            System.out.println(x + ",");
+            System.out.print(x + ",");
     }
     
 }
