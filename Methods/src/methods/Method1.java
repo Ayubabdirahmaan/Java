b7dc81d0dcd82a2df04e12f5b4cab28919bc3da9
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-08-28 21:03:08 +0300

    Added

--- Methods/src/methods/Method1.java
+++ Methods/src/methods/Method1.java
@@ -1,11 +1,23 @@
 package methods;
+
+import java.util.Calendar;
+
 public class Method1 {
-    static  String name = "Eng Abdikafi";
-    public void dislpay () {
-        System.out.println("Waan ku salaamay" + name);
+//  static  String name = "eng ayub";
+//    public String display() {
+////        System.out.println("Waan ku salamay " + name);
+//        return "Waan ku salamay " + name;
+//    }
+//    
+//    public static void show() {
+//        System.out.println("Hi " + name);
+//    }
+    public int  withRetun(int birthYear) {
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int age = year - birthYear;
+        return  age;
     }
-    public static void show(String name) {
-        System.out.println("hi" + name);
-    }
+    
+   
     
 }
--- Methods/src/methods/Methods.java
+++ Methods/src/methods/Methods.java
@@ -1,9 +1,17 @@
 package methods;
 public class Methods {
     public static void main(String[] args) {
- Method1.show("ayub");
-//new Array1();
-new Array2();
+//        Method1.show();
+//        Method1 obj = new Method1();
+//        obj.display();
+        Method1 obj = new Method1();
+        
+        if(obj.withRetun(2010) >= 18){
+            System.out.println("You can learn University");
+        }else {
+            System.out.println("you are young");
+        }
+        
     }
     
 }
