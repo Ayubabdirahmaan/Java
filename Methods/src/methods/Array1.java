4c7db677d0a2b9f42ae691d8d90cd9c89cd6720c
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-08-29 21:52:26 +0300

    Added

--- Methods/src/methods/Array1.java
+++ Methods/src/methods/Array1.java
@@ -2,14 +2,13 @@ package methods;
 import java.util.Scanner;
 public class Array1 {
         Scanner kb = new Scanner(System.in);
-        int num [];
-    public Array1() {
-        num = new int [3];
-        for(int i=0; i<num.length-1; i+=1) {
-             System.out.println("Enter number 1" + (i+1)+ ",");
-                num[i] = kb.nextInt();
-        }
-        num[num.length-1] = num[0] + num[1];
-        System.out.println("Total is " + num[num.length-1]);
-    }
+        int tiro [];
+     public Array1() {
+       tiro = new int[3];
+       for(int i=0; i<tiro.length; i+=1) {
+           System.out.println("Enter number 1");
+          tiro[i] = kb.nextInt();
+          tiro[tiro.length-1]+=tiro[i];
+       }
+     }
 }
