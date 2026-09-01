a07ed1cfddbe8da38959b0757a1052926286b60a
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-09-01 21:31:42 +0300

    Added

--- Methods/src/methods/Array7.java
+++ Methods/src/methods/Array7.java
@@ -3,7 +3,7 @@ public class Array7 {
     String names[] = {"Ayub","Mohamed","Fartun","Jamac","Abdiwali"};
     public Array7() {
         for(int i=0; i< names.length; i+=1) {
-            System.out.println(names[i]);
+            System.out.print(names[i] + " , ");
         }
     }
 }
