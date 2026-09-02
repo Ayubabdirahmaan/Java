de2c2aede46be0f81cf0b8750054e6dcb0f24a80
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-09-02 14:50:28 +0300

    Added

--- IntermediateLevel/src/intermediatelevel/Arrays.java
+++ IntermediateLevel/src/intermediatelevel/Arrays.java
@@ -1,17 +1,11 @@
 package intermediatelevel;
 import java.util.Scanner;
 public class Arrays {
-    Scanner kb = new Scanner(System.in);
-    int num [];
     public Arrays() {
-        System.out.println("How many Persons");
-        int n = kb.nextInt();
-        for(int i=0;  i< n; i+=1) {
-            System.out.println("Enter birthYear of person");  
-            num[i] = kb.nextInt();
-            num[2] = num[0] + num[1];
+        for(int i = 1; i<10; i+=1) {
+            for(int b=1; b<10; b+=1)
+                System.out.print(b + "\t");
+            System.out.println("");
         }
-        System.out.println("total is " + num[2]);
     }
-    
 }
