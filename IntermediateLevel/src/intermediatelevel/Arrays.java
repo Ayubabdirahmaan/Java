04a68abd82d691ce26f927c2da5a800bb8016303
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-09-02 14:15:14 +0300

    added

--- IntermediateLevel/src/intermediatelevel/Arrays.java
+++ IntermediateLevel/src/intermediatelevel/Arrays.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Arrays {
     Scanner kb = new Scanner(System.in);
     int num [];
-    int year = 2026;
     public Arrays() {
-        num = new int[3];
-        for(int i=0; i<num.length-1; i+=1) {
-            System.out.println("Enter Your Age");
+        System.out.println("How many Persons");
+        int n = kb.nextInt();
+        for(int i=0;  i< n; i+=1) {
+            System.out.println("Enter birthYear of person");  
             num[i] = kb.nextInt();
-            num[0] =  num[1] - year;
+            num[2] = num[0] + num[1];
         }
-        System.out.println("Your Age: " + num[0]);
+        System.out.println("total is " + num[2]);
     }
     
 }
