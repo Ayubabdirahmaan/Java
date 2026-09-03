a22e84c82c7a5188762dbb767e4331f7fdb51ab9
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-09-03 14:54:42 +0300

    Added

--- IntermediateLevel/src/intermediatelevel/Arrays.java
+++ IntermediateLevel/src/intermediatelevel/Arrays.java
@@ -1,11 +1,16 @@
 package intermediatelevel;
 import java.util.Scanner;
 public class Arrays {
+    Scanner kb = new Scanner(System.in);
+    int tiro [];
     public Arrays() {
-        for(int i = 1; i<10; i+=1) {
-            for(int b=1; b<10; b+=1)
-                System.out.print(b + "\t");
-            System.out.println("");
-        }
+       tiro = new int [3];
+       for(int i=0; i< tiro.length-1; i+=1) {
+           System.out.println("Enter Number " + (i+1) + " : ");
+          tiro[i] = kb.nextInt();
+          tiro[tiro.length-1] +=tiro[i];
+       }
+        System.out.println("Total is" + tiro[tiro.length-1] );
+       
     }
 }
