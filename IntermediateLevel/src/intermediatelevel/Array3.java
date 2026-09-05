9e6e6436461184b2dda0fdcad3639ce40ea11005
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-09-05 09:34:04 +0300

    Added

--- IntermediateLevel/src/intermediatelevel/Array3.java
+++ IntermediateLevel/src/intermediatelevel/Array3.java
@@ -3,4 +3,15 @@ import java.util.Scanner;
 public class Array3 {
     Scanner kb = new Scanner(System.in);
     int tiro[][];
+    public Array3() {
+        System.out.println("Meeqo qof");
+        int n = kb.nextInt();
+        tiro = new int [n][n];
+        for(int i=0; i<tiro.length; i+=1) {
+            System.out.println("Enter Your birthday");
+            tiro[i][i] = kb.nextInt();
+            tiro[tiro.length] 
+        }
+        
+    }
 }
--- IntermediateLevel/src/intermediatelevel/IntermediateLevel.java
+++ IntermediateLevel/src/intermediatelevel/IntermediateLevel.java
@@ -4,7 +4,8 @@ public class IntermediateLevel {
     public static void main(String[] args) {
         Arrays obj = new Arrays();
 //        obj.Arrays();
-            new Array1();
+//            new Array1();
+new Array3();
     }
     
 }
