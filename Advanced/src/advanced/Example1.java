68b672065fb67ada9b0bae6e743de5985801fc38
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-09-15 11:55:09 +0300

    added

--- Advanced/src/advanced/Example1.java
+++ Advanced/src/advanced/Example1.java
@@ -10,7 +10,7 @@ public class Example1 {
            if(type.equals("file"))
                System.out.println(fi.createNewFile()? "Create file:" : "not created file");
            else if(type.equals("folder"))
-               System.out.println(fi.mkdir()? "folder created:" : "not created folder");
+               System.out.println(fi.mkdir()? "folder created:" : "not created folder:");
        }catch(Exception ex) {
            System.out.println(ex.getMessage());
        }
