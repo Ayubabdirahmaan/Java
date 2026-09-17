4b2a1f1924d61da456284122b1581b194444a9b4
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-09-17 12:56:26 +0300

    added

---
+++ Production/src/production/Example1.java
@@ -0,0 +1,6 @@
+package production;
+public class Example1 {
+    public Example1() {
+        System.out.println("Say hello!");
+    }
+}
---
+++ Production/src/production/Production.java
@@ -0,0 +1,7 @@
+package production;
+public class Production {
+    public static void main(String[] args) {
+        new Example1();
+    }
+    
+}
