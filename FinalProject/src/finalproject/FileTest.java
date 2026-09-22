1507535580a02317141e19691f3c143da57398b5
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-09-22 10:34:20 +0300

    Added

---
+++ FinalProject/src/finalproject/FileTest.java
@@ -0,0 +1,9 @@
+package finalproject;
+import java.io.File;
+public class FileTest {
+    public void FileCreation(String path, String type) {
+        File fi= new File(path);
+        if(type.equals('file'))
+        System.out.println("");
+    }
+}
