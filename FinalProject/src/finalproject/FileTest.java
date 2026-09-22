8b6c13c2e0ceb30a743b9c3d87a8eb91d8993538
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-09-22 10:46:39 +0300

    Added

--- FinalProject/src/finalproject/FileTest.java
+++ FinalProject/src/finalproject/FileTest.java
@@ -2,8 +2,17 @@ package finalproject;
 import java.io.File;
 public class FileTest {
     public void FileCreation(String path, String type) {
-        File fi= new File(path);
-        if(type.equals('file'))
-        System.out.println("");
+      try{
+            File fi= new File(path);
+        if(type.equals("file"))
+            System.out.println(fi.createNewFile()? "creation file": "not creation file");
+        else if(type.equals("folder"))
+            System.out.println(fi.mkdir()? "creation folder" : "not creation folder");
+      }catch(Exception ex) {
+          System.out.println(ex.getMessage());
+      }
+    }
+    public FileTest() {
+        FileCreation("C:\\Users\\hp\\Desktop\\Salmaan\\music\\show.MVW","file");
     }
 }
--- FinalProject/src/finalproject/FinalProject.java
+++ FinalProject/src/finalproject/FinalProject.java
@@ -2,8 +2,8 @@
 package finalproject;
 public class FinalProject {
     public static void main(String[] args) {
-        FileNames ob= new FileNames();
-        ob.InsertFiles("C:\\Users\\hp\\Desktop\\Salmaan/mode.php", "file");
+     new FileTest();
+       
     }
     
 }
