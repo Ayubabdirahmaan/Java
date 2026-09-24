5986f228965e5b0a91c854fe95156652c0d65490
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-09-24 12:26:46 +0300

    Added

---
+++ FinalProject/src/finalproject/Array.java
@@ -0,0 +1,9 @@
+package finalproject;
+public class Array {
+    String names[] = {"ayub","abdi","mohamed"};
+           public Array() {
+                for(String magac: names)
+                    System.out.println("ASC " + magac);
+            }
+    
+}
