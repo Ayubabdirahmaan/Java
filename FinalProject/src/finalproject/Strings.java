8cc09e6ae724b4e3176a3db6cd00cad1a3b9d1bb
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-09-24 16:23:38 +0300

    Added

--- FinalProject/src/finalproject/Strings.java
+++ FinalProject/src/finalproject/Strings.java
@@ -8,7 +8,9 @@ public class Strings {
 //        System.out.println(user.toLowerCase()); this is tolowercase
 //        System.out.println(user.toUpperCase()); this is to uppercase
 //            System.out.println(user.length());   this is length of string
-            System.out.println(user.concat("Welcome "));
+//            System.out.println(user.concat("Welcome "));  waxaa uu isku kaa dhajinayaa laba string
+//                    System.out.println(user.indexOf("M"));   eray index uu ku jiro ayuu soo sheegayaa
+                    System.out.println(user.lastIndexOf("S"));
     }
     public Strings() {
         input();
